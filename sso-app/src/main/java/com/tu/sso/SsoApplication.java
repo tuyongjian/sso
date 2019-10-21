31a9f770bc66b0ff6e4a018da26c6d087550b74b
HQ01UG982 <hq01ug982@mb.com>
2019-10-21 15:18:38 +0800

    sso study

---
+++ sso-app/src/main/java/com/tu/sso/SsoApplication.java
@@ -0,0 +1,17 @@
+package com.tu.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+class SsoApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SsoApplication.class);
+    }
+}
---
+++ sso-controller/src/main/java/com/tu/sso/controller/SsoAction.java
@@ -0,0 +1,9 @@
+package com.tu.sso.controller;
+
+/**
+ * @Auther: tuyongjian
+ * @Date: 2019/10/21 15:18
+ * @Description:
+ */
+public class SsoAction {
+}
