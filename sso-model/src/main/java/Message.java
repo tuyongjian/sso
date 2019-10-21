5eba3956eba1d4b97c579f1c020babf557447142
HQ01UG982 <hq01ug982@mb.com>
2019-10-21 15:32:30 +0800

    sso study

---
+++ sso-model/src/main/java/Message.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: tuyongjian
+ * @Date: 2019/10/21 15:31
+ * @Description:
+ */
+public class Message {
+}
---
+++ sso-service/src/main/java/com/tu/sso/service/ISsoService.java
@@ -0,0 +1,9 @@
+package com.tu.sso.service;
+
+/**
+ * @Auther: tuyongjian
+ * @Date: 2019/10/21 15:31
+ * @Description:
+ */
+public interface ISsoService {
+}
