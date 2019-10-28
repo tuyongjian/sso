ada7116e8857483998bf3262288acdf4206524e3
HQ01UG982 <hq01ug982@mb.com>
2019-10-28 20:02:52 +0800

    sso study

--- sso-resource/src/main/java/com/tu/sso/resources/config/OAuth2ResourceConfig.java
+++ sso-resource/src/main/java/com/tu/sso/resources/config/OAuth2ResourceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -64,5 +65,6 @@ public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
              .anyRequest()
              .authenticated();
 
-         }
+    }
+
 }
--- sso-server/src/main/java/com/tu/sso/server/config/WebSecurityConfig.java
+++ sso-server/src/main/java/com/tu/sso/server/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,6 +40,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").authenticated()    //其他目录需要认证
                 .and()
                 .httpBasic();                          //开启基本http验证
+    }
+
+    //忽略校验的内容
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favor.ioc");
     }
 
     // 把 PasswordEncoder 放到  Spring 容器中
